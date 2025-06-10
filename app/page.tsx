c7f73347ab72f54d2e7145dedf494e370ab546f8
dev mehta <mehtadev2004@gmail.com>
2025-06-10 18:16:18 +0530

    nb

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div className='text-2xl font-karla'>
+      hii welcome to my new project
+    </div>
   )
 }
 
