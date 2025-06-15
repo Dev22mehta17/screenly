5d8438d02c0e05e952bc26b43f2273afa7c63a07
dev mehta <mehtadev2004@gmail.com>
2025-06-16 01:03:38 +0530

    sd

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 const Page = () => {
   return (
     <main className="wrapper page">
+      <Header title="All Videos" subHeader='Public Library' />
       hii welcome to my new project
     </main>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
 
-const Header = () => {
+const Header = ({subHeader,title,userImg}:SharedHeaderProps) => {
   return (
-    <div>Header</div>
+    <header className="header">
+        <section className="header-container">
+            <div className="details">
+                {userImg && (
+                    <Image src={userImg || 'assets/images/dummy.jpeg'} alt="user" width={66} height={66} className="rounded-full"/>
+                    )
+                }
+                <article>
+                    <p>{subHeader}</p>
+                    <h1>{title}</h1>
+                </article>
+            </div>
+            <aside>
+                <link href="/upload">
+                    {/* <Image src="/assets/icons/upload.svg" alt="upload"width={16} height={16}/> */}
+                    <span>Upload A Video</span>
+                </link>
+            </aside>
+        </section>
+    </header>
   )
 }
 
